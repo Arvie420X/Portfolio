2592e2d326def4943f8ffadfe154b3dff85f45f2
Arvie420X <arvidaslobaton420x@gmail.com>
2023-09-06 23:46:22 +0800

    changing my about component and adding css style

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,68 +1,81 @@
-
 // countup
 // import CountUp from 'react-countup'
 // intersection observer hook
 // import { useInView } from 'react-intersection-observer';
-// motion 
-import { motion } from 'framer-motion';
+// motion
+import { motion } from "framer-motion";
 // variant
-import { fadeIn } from '../utils/variants';
+import { fadeIn } from "../utils/variants";
 
-import aboutImg from '../assets/SpaceXCrew.png'
+import aboutImg from "../assets/SpaceXCrew.png";
 
-import Experience from './Experience'
+import Experience from "./Experience";
 
 const About = () => {
-
   // const [ref, inView] = useInView({
   //   thereshold: 0.5,
   // });
   return (
-    <div id='about' className='bg-black flex flex-col items-center justify-center min-h-screen h-auto'>
-
-      <div className='w-full md:w-[82%] min-h-screen h-auto mt-32 md:mt-10'>
-
-      <motion.div 
-        variants={fadeIn('left', 0.5)}
-        initial='hidden'
-        whileInView={'show'}
-        viewport={{ once: false, amount: 0.3 }} 
-        className='flex justify-start md:justify-end items-center text-white text-5xl md:text-8xl ml-8 md:ml-0 mb-2'>
-          <h1 className='front'>About</h1>
+    <div
+      id="about"
+      className="bg-black flex flex-col items-center justify-center min-h-screen h-auto"
+    >
+      <div className="w-full md:w-[82%] min-h-screen h-auto mt-32 md:mt-10">
+        <motion.div
+          variants={fadeIn("left", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="flex justify-start md:justify-end items-center text-white text-5xl md:text-8xl ml-8 md:ml-0 mb-2"
+        >
+          <h1 className="front">About</h1>
         </motion.div>
 
-        <div className='flex flex-col lg:flex-row text-white mt-24 lg:mt-0 md:space-y-10'>
+        <div className="flex flex-col lg:flex-row text-white mt-24 lg:mt-0 md:space-y-10">
           <motion.div
-            variants={fadeIn('down', 0.5)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{ once: false, amount: 0.3 }} 
-            className='flex bg-contain bg-no-repeat h-[450px] md:h-[640px] lg:h-auto w-full lg:w-1/2 mix-blend-lighten'>
-            <img className='h-[400px] md:h-[640px] lg:h-auto w-full p-2 object-cover mix-blend-lighten rounded-3xl' src={aboutImg} alt='aboutImage' />
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex bg-contain bg-no-repeat h-[450px] md:h-[640px] lg:h-auto w-full lg:w-1/2 mix-blend-lighten"
+          >
+            <img
+              className="h-[400px] md:h-[640px] lg:h-auto w-full p-2 object-cover mix-blend-lighten rounded-3xl"
+              src={aboutImg}
+              alt="aboutImage"
+            />
           </motion.div>
           <motion.div
-            variants={fadeIn('left', 0.5)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{ once: false, amount: 0.3 }} 
-            className='flex flex-col justify-center lg:pl-8 lg:w-1/2'>
-            <h1 className='front text-center text-gradient lg:text-left text-base px-7 md:text-xl'>
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex flex-col justify-center lg:pl-8 lg:w-1/2"
+          >
+            {/* <h1 className='front text-center text-gradient lg:text-left text-base px-7 md:text-xl'>
               Driven by a strong passion for technology, I am a career shifter eagerly seeking fresh opportunities to discover. I have dedicated my time to acquiring knowledge in web development. To achieve this, I pursued a combination of bootcamp training and self-study. While my focus has primarily been on the MERN stack, I remain open to exploring other technologies as well. Throughout my learning journey, I have discovered that my true interest lies in back-end development.           
+            </h1> */}
+            <h1 className="about front text-center text-gradient lg:text-left text-base px-7 md:text-xl">
+              Welcome to my digital universe! I am Arvidas a career shifter,
+              navigating the endless possibilities of web and app development.
+              Ever since I developed a strong interest in technology through
+              reading and staying updated on the latest tech trends, I am now
+              taking concrete steps towards a career in the tech industry. To
+              turn my dream of becoming a developer into reality, I employ two
+              distinct approaches: a bootcamp and self-study. Although my tech
+              knowledge is centered around the MERN stack. I remain open to
+              exploring other technologies as well, and the first on my list is
+              probably Python and PostgreSQL.
             </h1>
           </motion.div>
         </div>
 
-
         <div>
           <Experience />
         </div>
-
       </div>
-
-      
-      
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
